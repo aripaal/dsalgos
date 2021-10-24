eb311c75693684a70000d649af15033857ff7b05
i350746 <arijit.pal@sap.com>
2021-10-24 15:18:01 +0530

    design patterns

--- src/main/java/designpatterns/FactoryPattern.java
+++ src/main/java/designpatterns/FactoryPattern.java
@@ -1,4 +1,67 @@
 package designpatterns;
 
+import java.util.*;
+
 public class FactoryPattern {
+    static Pet petFactory(String animalType) {
+        if (animalType.equals("cat"))
+            return new Cat();
+        else if (animalType.equals("dog"))
+            return new Dog();
+        else return null;
+    }
+
+    public static void main(String... args) {
+        List<Pet> pets = Arrays.asList(petFactory("cat"), petFactory("dog"), petFactory("cat"));
+        for (Pet p : pets) {
+            p.drink();
+            p.eat();
+            p.sleep();
+        }
+
+    }
+}
+
+interface Pet {
+    void eat();
+
+    void sleep();
+
+    void drink();
+}
+
+class Dog implements Pet {
+
+    @Override
+    public void eat() {
+        System.out.println("Dog eating bones");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Dog sleeping on mat");
+    }
+
+    @Override
+    public void drink() {
+        System.out.println("Dog drinking water");
+    }
+}
+
+class Cat implements Pet {
+
+    @Override
+    public void eat() {
+        System.out.println("Cat eating fish");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Cat sleeping on bed");
+    }
+
+    @Override
+    public void drink() {
+        System.out.println("Cat drinking milk");
+    }
 }
---
+++ src/main/java/designpatterns/Filter.java
@@ -0,0 +1,46 @@
+package designpatterns;
+
+import lombok.*;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class Filter {
+    public static void main(String... args) {
+        List<Employee> employees = Arrays.asList(new Employee(22, "Swetha", Sex.F),
+                new Employee(27, "Ravi", Sex.M),
+                new Employee(27, "Hemanth", Sex.M),
+                new Employee(32, "Jovi", Sex.M),
+                new Employee(25, "Heena", Sex.F),
+                new Employee(21, "Kranti", Sex.F),
+                new Employee(20, "Aman", Sex.M));
+
+        Females f = new Females();
+        List<Employee> employeeList = f.filter(employees);
+        System.out.println(employeeList);
+    }
+}
+
+@AllArgsConstructor
+@ToString
+class Employee {
+    int age;
+    String name;
+    Sex sex;
+}
+
+enum Sex {M, F};
+
+interface Criteria {
+    List<Employee> filter(List<Employee> employees);
+}
+
+class Females implements Criteria {
+    @Override
+    public List<Employee> filter(List<Employee> employees) {
+        return employees
+                .stream()
+                .filter(e -> e.sex == Sex.F)
+                .collect(Collectors.toList());
+    }
+}
